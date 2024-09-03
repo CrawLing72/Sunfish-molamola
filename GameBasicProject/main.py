83f961906f568e630d12fb9e00bad85d896b3fe1
CrawLing72 <caseus.molamola@gmail.com>
2024-09-03 23:10:07 +0900

    Basic Main Code Added

--- GameBasicProject/main.py
+++ GameBasicProject/main.py
@@ -6,11 +6,11 @@ import pygame
 pygame.init()
 pygame.display.set_caption('Frieren : Beyond Journey')
 
-myscreen = pygame.display.set_mode((500,500))
+myscreen = pygame.display.set_mode((1600,900))
 myTextFont = pygame.font.SysFont('Arial', 50)
 myText = myTextFont.render('Frieren', True, (255,255,255))
 myTextRect = myText.get_rect()
-myTextRect.center = (250, 250)
+myTextRect.center = (800, 450)
 
 while True:
     myscreen.fill((0,0,0))
