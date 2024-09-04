a861fbddc4b913abd2a63616d65dac3784775020
CrawLing72 <caseus.molamola@gmail.com>
2024-09-04 17:55:31 +0900

    button modified

--- GameBasicProject/main.py
+++ GameBasicProject/main.py
@@ -10,9 +10,12 @@ pygame.display.set_caption('Frieren : Beyond Journey')
 
 myScreen = pygame.display.set_mode((globs.WINDOW_WIDTH,globs.WINDOW_HEIGHT))
 
+def sayhello():
+    print("Hello, world!")
+
 helloWolrd = probs.Text("Hello, world!", globs.COMMON_FONT)
 elainaImage = probs.Image("./Resources/Animation/Elaina_Sliced/images/Elaina_Right_01.png")
-elainaButton = probs.Button("./Resources/Animation/Elaina_Sliced/images/Elaina_Right_01.png", print("Hello, Button"))
+elainaButton = probs.Button("./Resources/Animation/Elaina_Sliced/images/Elaina_Right_01.png", sayhello)
 
 #Game Loop
 while True:
