be38b72443faac04cb68f68edb6e26ef476ea249
CrawLing72 <caseus.molamola@gmail.com>
2024-09-08 12:29:32 +0900

    Main Menu Added!

--- GameBasicProject/main.py
+++ GameBasicProject/main.py
@@ -6,33 +6,45 @@ import system
 
 #pygame initialization
 pygame.init()
-pygame.display.set_caption('Frieren : Beyond Journey')
+pygame.display.set_caption('Sunfish : Mola Mola')
 
 myScreen = pygame.display.set_mode((globs.WINDOW_WIDTH,globs.WINDOW_HEIGHT))
 
-def sayhello():
-    print("Hello, world!")
-
-helloWolrd = probs.Text("Hello, world!", globs.COMMON_FONT)
-elainaImage = probs.Image("./Resources/Animation/Elaina_Sliced/images/Elaina_Right_01.png")
-elainaButton = probs.Button("./Resources/Animation/Elaina_Sliced/images/Elaina_Right_01.png", sayhello)
-
 #Game Loop
-while True:
-    myScreen.fill(globs.black)
-    elainaButton.draw(globs.WINDOW_WIDTH/2, globs.WINDOW_HEIGHT/2, myScreen)
+def rungame():
+    while True:
+        myScreen.fill(globs.black)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            elainaButton.detection()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    keyEvent = pygame.key.get_pressed()
-    if keyEvent[pygame.K_w]:
-        print("W Released!")
-    elif keyEvent[pygame.K_s]:
-        print("S Released!")
+        keyEvent = pygame.key.get_pressed()
+        if keyEvent[pygame.K_w]:
+            print("W Released!")
+        elif keyEvent[pygame.K_s]:
+            print("S Released!")
 
-    pygame.display.update()
+        pygame.display.update()
+
+
+def main_menu():
+    button = probs.Button("./Resources/Animation/Elaina_Sliced/images/Elaina_Right_01.png", rungame)
+    while True:
+        myScreen.fill(globs.black)
+
+        button.draw(globs.WINDOW_WIDTH/2, globs.WINDOW_HEIGHT/2, myScreen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                button.detection()
+
+        pygame.display.update()
+
+main_menu()
+
+
