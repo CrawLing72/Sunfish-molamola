a012cf0c4fe52d0f1971abb9be18eb866dfdde79
CrawLing72 <caseus.molamola@gmail.com>
2024-09-10 22:08:40 +0900

    Character Animation System Added

--- GameBasicProject/globs.py
+++ GameBasicProject/globs.py
@@ -1,5 +1,6 @@
-import pygame
-from pygame.examples.grid import TILE_SIZE
+import random
+import probs
+import time
 
 #Screen Parameter
 WINDOW_WIDTH = 1280
@@ -20,10 +21,20 @@ CHUNK_SIZE = 24
 PLAYER_SPEED = 5
 
 ## Env Related
-WORLD_SEED = 12
+WORLD_SEED = random.randint(0, 300)
 FPS = 60
 
 # Tile Path
 SEA = "./Resources/imgs/images/normalsea.png"
 GROUND = "./Resources/imgs/images/ground.png"
 DEEPSEA = "./Resources/imgs/images/deepsea.png"
+
+
+
+# ANIMATION PATH
+w_path = [probs.Image(f"./Resources/imgs/images/Elaina_Rung_Back_0{i}.png") for i in range(1, 7)]
+s_path = [probs.Image(f"./Resources/imgs/images/Elaina_Run_Fr_0{i}.png") for i in range(1, 7)]
+d_path = [probs.Image(f"./Resources/imgs/images/Elaina_Run_lf_0{i}.png") for i in range(1, 7)]
+a_path = [probs.Image(f"./Resources/imgs/images/Elaina_Run_Rt_0{i}.png") for i in range(1, 7)]
+waiting_path = [probs.Image(f"./Resources/imgs/images/Elaina_Front_0{i}.png") for i in range(1, 5)]
+ANIPATH = {"w" : w_path, "s" : s_path, "a" : a_path, "d" : d_path, "e" : waiting_path}
--- GameBasicProject/main.py
+++ GameBasicProject/main.py
@@ -10,7 +10,7 @@ import system
 pygame.init()
 pygame.display.set_caption('Sunfish : Mola Mola')
 
-myScreen = pygame.display.set_mode((globs.WINDOW_WIDTH,globs.WINDOW_HEIGHT), pygame.FULLSCREEN)
+myScreen = pygame.display.set_mode((globs.WINDOW_WIDTH,globs.WINDOW_HEIGHT))
 
 clock = pygame.time.Clock()
 
@@ -21,10 +21,14 @@ def rungame():
     player_x, player_y = globs.WINDOW_WIDTH/2, globs.WINDOW_HEIGHT/2
     world_offset_x, world_offset_y = 0, 0
 
-    sunfish = probs.Image("./Resources/imgs/images/ocean_sunfish_128.png")
+    sunfish = probs.Character(globs.ANIPATH,8, player_x, player_y)
+    Coordinate_text = probs.Text("X : ???, Y : ???", globs.COMMON_FONT, 20, globs.WHITE)
+
+    delta_time = clock.tick(60) / 1000.0
 
 
     while True:
+        movement_vector = [0, 0]
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -32,18 +36,25 @@ def rungame():
 
         keyEvent = pygame.key.get_pressed()
         if keyEvent[pygame.K_w]:
-            world_offset_y += globs.PLAYER_SPEED
+            world_offset_y += globs.PLAYER_SPEED * delta_time
+            movement_vector[1] = globs.PLAYER_SPEED
         elif keyEvent[pygame.K_s]:
-            world_offset_y -= globs.PLAYER_SPEED
+            world_offset_y -= globs.PLAYER_SPEED * delta_time
+            movement_vector[1] = -globs.PLAYER_SPEED
         elif keyEvent[pygame.K_a]:
-            world_offset_x += globs.PLAYER_SPEED
+            world_offset_x += globs.PLAYER_SPEED * delta_time
+            movement_vector[0] = globs.PLAYER_SPEED
         elif keyEvent[pygame.K_d]:
-            world_offset_x -= globs.PLAYER_SPEED
+            world_offset_x -= globs.PLAYER_SPEED * delta_time
+            movement_vector[0] = -globs.PLAYER_SPEED
 
         myScreen.fill(globs.BLACK)
 
         world.draw(myScreen, player_x, player_y, world_offset_x, world_offset_y)
-        sunfish.draw(myScreen, globs.WINDOW_WIDTH/2, globs.WINDOW_HEIGHT/2)
+        sunfish.draw(myScreen, movement_vector[0], movement_vector[1], delta_time)
+
+        Coordinate_text.string = f"X: {-int(world_offset_x/globs.TILE_SIZE)}, Y: {int(world_offset_y/globs.TILE_SIZE)}"
+        Coordinate_text.draw(myScreen, 100, 25)
 
         pygame.display.update()
 
